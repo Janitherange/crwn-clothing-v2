05306956b99ea67ee6f191dd20debf75b83fb0b3
Janith Eranga <31025649+Janitherange@users.noreply.github.com>
2022-08-21 12:20:28 +0530

    added in our addToCart feature

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,13 +1,22 @@
 import "./card-dropdown.styles.scss"
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component.jsx";
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context.jsx"
 
 const CardDropdown = () => {
-  return(
-       <div className="cart-dropdown-container">
-            <div className="cart-items"/>
-           <Button>GO TO CHECKOUT</Button>
-       </div>
-  )
+    const {cartItems} = useContext(CartContext)
+
+    return (
+        <div className="cart-dropdown-container">
+            <div className="cart-items">
+                {
+                    cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)
+                }
+            </div>
+            <Button>GO TO CHECKOUT</Button>
+        </div>
+    )
 }
 
 export default CardDropdown;
