3f490e5b2b3ff81d79652e70928c97a2563e4ca9
Janith Eranga <31025649+Janitherange@users.noreply.github.com>
2022-08-21 09:41:04 +0530

    Adding cart context and shopping icon

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom';
+
+import App from './App';
+import {UserProvider} from './contexts/user.context';
+import {ProductsProvider} from './contexts/products.context';
+
+import './index.scss'
+import {CartProvider} from "./contexts/cart.context";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <UserProvider>
+                <ProductsProvider>
+                    <CartProvider>
+                        <App/>
+                    </CartProvider>
+                </ProductsProvider>
+            </UserProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+)
